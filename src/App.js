c28018c6ceaf70ee412ee3cbc5557f030de616d6
Sandeep M S <sandeepms746@gmail.com>
2023-02-16 17:04:15 +0530

    changed code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import React from "react";
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import React from 'react'
+import { ImWhatsapp } from 'react-icons/im'
+import Backdrop from '@mui/material/Backdrop'
+import Modal from '@mui/material/Modal'
+import Button from '@mui/material/Button'
+import Fade from '@mui/material/Fade'
+import Box from '@mui/material/Box'
+import { RxCross2 } from 'react-icons/rx'
+import IconButton from '@mui/material/IconButton'
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '80%',
+  height: '80%',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+}
 
 function App() {
+  const [open, setOpen] = React.useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
   return (
     <div className="flex flex-col font-body">
       <Navbar />
@@ -20,7 +45,7 @@ function App() {
               Did you know that LearnCab is a unique
               <span className="font-bold">
                 #studyanytimeanywhereonanydevice
-              </span>{" "}
+              </span>{' '}
               online coaching platform for CMA students that works on phones,
               tablets, and computers?
             </p>
@@ -58,7 +83,7 @@ function App() {
                         alt=""
                         className="max-h-10"
                       />
-                    </span>{" "}
+                    </span>{' '}
                     Downloadable notes
                   </li>
                   <li className="flex flex-row gap-x-4 items-center mb-3">
@@ -68,7 +93,7 @@ function App() {
                         alt=""
                         className="max-h-10"
                       />
-                    </span>{" "}
+                    </span>{' '}
                     Chapter-wise quizzes, wherever applicable
                   </li>
                 </li>
@@ -106,31 +131,84 @@ function App() {
             <div className="flex flex-row justify-center gap-x-4 mb-6 mt-4">
               <div class="border max-w-md sm:p-16 rounded overflow-hidden shadow-2xl">
                 <div class="px-4 py-2">
-                  <div class="font-bold sm:text-xl mb-10 text-start ml-1">Single Group</div>
-                  <p class="border-[#02ca028c] p-4 rounded bg-[#02ca2e5e] text-[#005312] sm:text-base">pay only ₹13,999</p>
+                  <div class="font-bold sm:text-xl mb-10 text-start ml-1">
+                    Single Group
+                  </div>
+                  <p class="border-[#02ca028c] p-4 rounded bg-[#02ca2e5e] text-[#005312] sm:text-base">
+                    pay only ₹13,999
+                  </p>
                 </div>
               </div>
               <div class="border max-w-md sm:p-16 rounded overflow-hidden shadow-2xl">
                 <div class="px-4 py-2">
-                  <div class="font-bold sm:text-xl mb-10 text-start ml-1"> Both Group</div>
-                  <p class="border-[#02ca028c] p-4 rounded bg-[#02ca2e5e] text-[#005312] sm:text-base">pay only ₹21,999</p>
+                  <div class="font-bold sm:text-xl mb-10 text-start ml-1">
+                    {' '}
+                    Both Group
+                  </div>
+                  <p class="border-[#02ca028c] p-4 rounded bg-[#02ca2e5e] text-[#005312] sm:text-base">
+                    pay only ₹21,999
+                  </p>
                 </div>
               </div>
             </div>
 
-            <p className="mt-2 text-center">
-              Do feel free to contact :{" "}
-              <span className="font-bold text-lg sm:text-xl">7483800690</span>
+            <p className="mt-2 flex flex-row items-center justify-center gap-x-4">
+              Call or Whatsapp :
+              <span className="flex flex-row gap-x-2 items-center justify-center font-bold text-lg sm:text-xl">
+                <ImWhatsapp />
+                <a href="tel:7483800690">7483800690</a>
+              </span>
             </p>
           </div>
         </div>
-        <button className="border-white bg-blue-500 p-1 mb-2 px-6 rounded-lg text-white">
-          <a href="https://learncab.com/">Contact us</a>
-        </button>
+
+        <div className="mb-5">
+          <button
+            data-modal-target="defaultModal"
+            data-modal-toggle="defaultModal"
+            className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-2xl p-5 px-8 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            type="button"
+            onClick={handleOpen}
+          >
+            Contact us
+          </button>
+        </div>
+      </div>
+
+      <div>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={open}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <Box sx={style}>
+              <span className='flex flex-row items-center justify-between mb-4'>
+                <p className='font-body text-xl text-gray-500'>Contact Form</p>
+                <span className='border rounded-3xl'>
+                  <IconButton onClick={handleClose}>
+                    <RxCross2 />
+                  </IconButton>
+                </span>
+              </span>
+              <iframe
+                width="100%"
+                height="90%"
+                src="https://offers.learncab.com/#/forms/47153D93"
+                title="Forms"
+              ></iframe>
+            </Box>
+          </Fade>
+        </Modal>
       </div>
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
